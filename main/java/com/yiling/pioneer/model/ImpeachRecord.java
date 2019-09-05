90d339a03ff7bd9f91105f8120e4177d4d063701
Xzio <1024508706@qq.com>
2019-09-05 18:37:24 +0800

    举报接口

---
+++ main/java/com/yiling/pioneer/model/ImpeachRecord.java
@@ -0,0 +1,17 @@
+package com.yiling.pioneer.model;
+
+import lombok.Data;
+
+/**
+ * @Author: xc
+ * @Date: 2019/9/5 16:56
+ * @Description: 举报实体类
+ **/
+@Data
+public class ImpeachRecord {
+    private int id;
+    private int reporteeUID;
+    private int sendUID;
+    private int articleID;
+    private String content;
+}
