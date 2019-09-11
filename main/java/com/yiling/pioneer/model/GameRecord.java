ccf4800e578ae903db08082aa05f1126eb74fd33
Xzio <1024508706@qq.com>
2019-09-11 19:26:26 +0800

    比赛记录实体类

---
+++ main/java/com/yiling/pioneer/model/GameRecord.java
@@ -0,0 +1,16 @@
+package com.yiling.pioneer.model;
+
+import lombok.Data;
+
+/**
+ * @Author: xc
+ * @Date: 2019/9/11 13:04
+ * @Description: 比赛记录
+ **/
+@Data
+
+public class GameRecord {
+    private int id;
+    private int uID;
+    private int cID;
+}
